1102022b848153ef83bb044751c951cd2c6c8557
SeyidAkif101 <sayidakhifia-mpa101@code.edu.az>
2026-01-21 08:47:35 +0400

    Front to Back

--- Simulation-End/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Simulation_End.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
